a2e392f371247042afd7352199c67b7450e4af73
David <david.nunez@oktana.io>
2021-06-22 19:46:06 -0400

    Updates

---
+++ precache-manifest.b4705eff54894648ecdcd19a7953e4d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0efa49d2959c9234df7e9a50e7f45a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ad51ffa597d3da5ec185",
+    "url": "/portfolio/static/css/main.1e9a8c41.chunk.css"
+  },
+  {
+    "revision": "ec0fd58d6d88e628e370",
+    "url": "/portfolio/static/js/2.410a6df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.410a6df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad51ffa597d3da5ec185",
+    "url": "/portfolio/static/js/main.0e381871.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "7d60812ba888ba7b0ba7a1c5ec9f7f91",
+    "url": "/portfolio/static/media/Flaticon.7d60812b.ttf"
+  },
+  {
+    "revision": "90c81f3455df779c6c88507d587b5b58",
+    "url": "/portfolio/static/media/Flaticon.90c81f34.eot"
+  },
+  {
+    "revision": "918bfead04245b056d2d21c063671911",
+    "url": "/portfolio/static/media/Flaticon.918bfead.svg"
+  },
+  {
+    "revision": "cc7d1a39a7bc3cb671e47e5827a45aef",
+    "url": "/portfolio/static/media/Flaticon.cc7d1a39.woff"
+  },
+  {
+    "revision": "eeda2305942411261472408844af5774",
+    "url": "/portfolio/static/media/Flaticon.eeda2305.woff2"
+  },
+  {
+    "revision": "fc118007a4d254854a86cbace0820fb1",
+    "url": "/portfolio/static/media/david.fc118007.png"
+  },
+  {
+    "revision": "2cb8dc8186e8827cca3b6beebe4ef1c6",
+    "url": "/portfolio/static/media/icomoon.2cb8dc81.eot"
+  },
+  {
+    "revision": "302fbc0aa5dcec40d385772d64baad2c",
+    "url": "/portfolio/static/media/icomoon.302fbc0a.svg"
+  },
+  {
+    "revision": "62f33c03d096cdc058166bd6fbf7e4d0",
+    "url": "/portfolio/static/media/icomoon.62f33c03.woff"
+  },
+  {
+    "revision": "bab8cf81a530d89b36f24a80649c5e25",
+    "url": "/portfolio/static/media/icomoon.bab8cf81.ttf"
+  },
+  {
+    "revision": "4bf754fc6beceb979acf92e37e0bac73",
+    "url": "/portfolio/static/media/post_3.4bf754fc.jpg"
+  },
+  {
+    "revision": "1707f68dc2da32ff76f80c7624531909",
+    "url": "/portfolio/static/media/post_4.1707f68d.jpg"
+  },
+  {
+    "revision": "094d7f5de32e55b7e7e1f34486cd348e",
+    "url": "/portfolio/static/media/post_6.094d7f5d.jpg"
+  }
+]);
