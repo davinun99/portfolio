edd08ec2409f09503393b1bba5115284d8a6d6e0
David <david.nunez@oktana.io>
2021-06-22 20:28:54 -0400

    favicon update and package update

--- src/components/PortfolioSection.js
+++ src/components/PortfolioSection.js
@@ -15,58 +15,58 @@ const PortfolioSection = () => {
                     <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_2.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_2.jpg"/>
+                            <img class="img-fluid" alt="Post2" src="images/post_2.jpg"/>
                         </a>
                     </div>
                     <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_3.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_3.jpg"/>
+                            <img class="img-fluid" alt="Post3" src="images/post_3.jpg"/>
                         </a>
                     </div>
                     <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_4.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_4.jpg"/>
+                            <img class="img-fluid" alt="Post4" src="images/post_4.jpg"/>
                         </a>
                     </div>
                     <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_5.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_5.jpg"/>
+                            <img class="img-fluid" alt="Post5" src="images/post_5.jpg"/>
                         </a>
                     </div>
 
                     <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_6.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_6.jpg"/>
+                            <img class="img-fluid" alt="Post6" src="images/post_6.jpg"/>
                         </a>
                     </div>
 
                     <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_2.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src="images/post_2.jpg"/>
+                            <img class="img-fluid" alt="Post2" src="images/post_2.jpg"/>
                         </a>
                     </div>
                     <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_3.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src={post3}/>
+                            <img class="img-fluid" alt="Post3" src={post3}/>
                         </a>
                     </div>
                     <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_4.jpg" class="item-wrap fancybox">
                         <span class="icon-search2"></span>
-                        <img class="img-fluid" src={post4}/>
+                        <img class="img-fluid" alt="Post4" src={post4}/>
                         </a>
                     </div>
 
                     <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
                         <a href="images/post_6.jpg" class="item-wrap fancybox">
                             <span class="icon-search2"></span>
-                            <img class="img-fluid" src={post6}/>
+                            <img class="img-fluid" alt="Post5" src={post6}/>
                         </a>
                     </div>
                 </div>
